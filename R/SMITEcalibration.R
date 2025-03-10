3544a80e67dba485955d177454edfc7ec4a3f0d7
hphughescraft <116124029+hphughescraft@users.noreply.github.com>
2025-03-10 15:38:19 -0400

    Added functionality for xval bs only
    You've been underestimated your model parameters. Use cross-validation only to assess model parameters.

--- R/SMITEcalibration.R
+++ R/SMITEcalibration.R
@@ -10,6 +10,8 @@
 #' @param acc Autocorrelation Coefficient; if noise is specified as 'red', it describes the degree of autocorrelation in the error term.
 #' @param eigenclean Describes how (if at all) the singular values should be truncated to 'clean' the inverse solution. If the value is between 0 and 1, it will remove singular values based on the cumulative variance explained. If the value is greater than 1, it will return that many singular values (from the highest order).
 #' @param alpha The significance level for the confidence interval (i.e., 0.05 = 95-percent confidence).
+#' @param xval Cross-validation window size.
+#' @param bs.errors TRUE or FALSE whether you want to perform bootstrapping with the error matrices.
 #' @return S - Confidence intervals on the singular values of the forward matrix,
 #' @return x - SMITE model parameters, or loadings, for each column of the forward matrix.
 #' @return bhat - Predicted target anomalies
@@ -18,7 +20,7 @@
 #' @return residuals.xval - Only cross-validated residuals from the calibration
 #' @return e - Relevant error metrics. It enumerates the Standard Error of Prediction (SEP), Root-Mean-Square-Error (RMSE), and the correlation coefficient (r).
 #' @return e.xval - Relevant error metrics for the cross-validated data points specifically.
-#' @return e.stats - alpha-confidence intervals for R and RMSE (non-cross-validated and cross-validated)
+#' @return e.stats - Confidence intervals for R and RMSE (non-cross-validated and cross-validated). The size of the confidence interval is based on alpha.
 #' @export
 #' @examples
 #' # Load data from Hughes et al. (in revision)
@@ -40,7 +42,7 @@
 
 
 SMITE.calib <- function(A, b, Ae = NULL, be = NULL, it = 10000, noise = "white", acc = NULL,
-                        eigenclean = NULL, alpha = 0.05, xval = NULL) {
+                        eigenclean = NULL, alpha = 0.05, xval = NULL, bs.errors = FALSE) {
 
 
   # =============================== #
@@ -106,36 +108,46 @@ SMITE.calib <- function(A, b, Ae = NULL, be = NULL, it = 10000, noise = "white",
     # Nested For-Loop: Ap allocation
     # =============================== #
 
-    if(noise == 'white') {
-      for(j in 1:ncol(Ap)) {
-        for(i in 1:nrow(Ap)) {
-          # Perturb each cell of G by the error defined in the corresponding cell of Ge
-          Ap[i,j] <- rnorm(n = 1, mean = A[i,j], sd = Ae[i,j])
-        }
-      }
-    } else if(noise == 'red') {
-
-      Ar <- matrix(data = NA, nrow = nrow(A), ncol = ncol(A))
-
-      for(j in 1:ncol(Ar)) {
-        for(i in 1:nrow(Ar)) {
-          if(i == 1) {
-            Ar[i,j] <- rnorm(1, 0, Ae[i,j])
-          } else {
-            Ar[i,j] <- (Ar[i - 1, j] * acc) + rnorm(1, 0, Ae[i,j])
+    if(bs.errors) { # If bootstrapping with errors, then perturb with errors
+      if(noise == 'white') {
+        for(j in 1:ncol(Ap)) {
+          for(i in 1:nrow(Ap)) {
+            # Perturb each cell of A by the error defined in the corresponding cell of Ae
+            Ap[i,j] <- rnorm(n = 1, mean = A[i,j], sd = Ae[i,j])
           }
         }
+      } else if(noise == 'red') {
+
+        Ar <- matrix(data = NA, nrow = nrow(A), ncol = ncol(A))
+
+        for(j in 1:ncol(Ar)) {
+          for(i in 1:nrow(Ar)) {
+            if(i == 1) {
+              Ar[i,j] <- rnorm(1, 0, Ae[i,j])
+            } else {
+              Ar[i,j] <- (Ar[i - 1, j] * acc) + rnorm(1, 0, Ae[i,j])
+            }
+          }
+        }
+
+        Ap <- A + Ar
+      } else {
+        stop("Please specify whether the noise is red or white.")
       }
 
-      Ap <- A + Ar
-    } else {
-      stop("Please specify whether the noise is red or white.")
+      for(i in 1:nrow(Ap)) {
+        bp[i] <- rnorm(n = 1, mean = b[i], sd = be[i])
+      }
+
+    } else { # Otherwise, just make it A and bootstrap will use only cross-validation
+      Ap <- A
+      bp <- b
     }
 
 
-    for(i in 1:nrow(Ap)) {
-      bp[i] <- rnorm(n = 1, mean = b[i], sd = be[i])
-    }
+
+
+
 
     Ap_norm <- apply(Ap, MARGIN = 2, FUN = function(x) (x - mean(x)) /  sd(x))
     bp_norm <- (bp - mean(bp)) / sd(bp)
@@ -217,10 +229,10 @@ SMITE.calib <- function(A, b, Ae = NULL, be = NULL, it = 10000, noise = "white",
 
     # Statistics
     r <- cor.test(bhat, bpx)$estimate
-    rmse <- sum(sqrt((bpx - bhat)^2)) / length(bhat)
+    rmse <- (sum(sqrt((bpx - bhat)^2)) / length(bhat)) * sd(b)
 
     r_xval <- cor.test(bpv, bhat_xval)$estimate
-    rmse_xval <- sum(sqrt((bpv - bhat_xval)^2)) / length(bhat_xval)
+    rmse_xval <- (sum(sqrt((bpv - bhat_xval)^2)) / length(bhat_xval)) * sd(b)
 
     k_list[[k]] <- list(
       "S" = S,
